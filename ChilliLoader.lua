8c7fcd400f74fe9b0e39c5ccef088e2eef2998e7
artem236261634 <artemvetlugin28@gmail.com>
2025-08-22 10:05:31 +0300

    ChilliLoader.lua

--- ChilliLoader.lua
+++ ChilliLoader.lua
@@ -1,37 +1,33 @@
--- ChilliLoader.lua
-local HttpService = game:GetService("HttpService")
+-- ChilliLoader.lua (проверка ключа и загрузка основного скрипта)
 
--- URL основного скрипта, который будет загружаться
-local scriptUrl = "https://raw.githubusercontent.com/tienkhanh1/spicy/main/Chilli.lua" -- Ссылка на твой основной скрипт
+-- Устанавливаем правильный ключ
+local correctKey = "123456" -- Ты можешь поменять его на свой уникальный ключ
 
 -- Функция для проверки ключа
 local function checkKey(inputKey)
-    local validKey = "123456" -- Пример ключа. Замените на свой уникальный ключ
-
-    if inputKey == validKey then
+    if inputKey == correctKey then
         return true
     else
         return false
     end
 end
 
--- Запрос на ввод ключа (можно сделать GUI или просто запрос в консоль)
+-- Просим пользователя ввести ключ (можно заменить на графическое окно для Roblox)
 print("Введите ключ для активации:")
-local inputKey = io.read() -- Чтение ключа (для консольных скриптов)
 
+-- Получаем ключ от игрока
+local inputKey = io.read()  -- В обычных случаях можно использовать GUI для ввода
+
+-- Проверка ключа
 if checkKey(inputKey) then
-    print("Ключ верный! Загружаю скрипт...")
-    
-    -- Попробуем получить и выполнить скрипт с указанного URL
-    local success, result = pcall(function()
-        -- Получаем код с GitHub
-        local scriptContent = game:HttpGet(scriptUrl)
+    print("Ключ верный! Загружаем скрипт...")
 
-        -- Выполняем полученный код
-        loadstring(scriptContent)()
+    -- Загружаем и выполняем основной скрипт с GitHub
+    local success, result = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/tienkhanh1/spicy/main/Chilli.lua"))()
     end)
 
-    -- Если произошла ошибка при загрузке, выводим сообщение об ошибке
+    -- Если произошла ошибка, выводим её
     if not success then
         warn("Ошибка при загрузке скрипта: " .. result)
     end
